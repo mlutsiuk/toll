e768752800f669bba46d15805a5587b4334828e0
mlutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2022-07-14 22:26:02 +0300

    Changed form fields.

--- src/components/TollCalculator/DieselCalculator.vue
+++ src/components/TollCalculator/DieselCalculator.vue
@@ -1,31 +1,55 @@
 <template>
-    <div>
-        <h3>{{ engineVolume }}</h3>
-        <text-field v-model="engineVolume" id="engine_volume" label="Об'єм двигуна"/>
-        <manufacture-date-select v-model:age="carAge"/>
-        <text-field v-model="carPrice" id="car_price" label="Ціна автомобіля"/>
+    <div class="row">
+        <div class="col-auto d-flex align-items-center pr-5">
+            <engine-type-radio v-model:engine-type="engineType"/>
+        </div>
+
+        <div class="col">
+            <label for="engine_volume">Ціна автомобіля</label>
+            <div class="input-group mb-2 mr-sm-2">
+                <input v-model="carPrice" id="car_price" type="text" class="form-control">
+                <div class="input-group-append">
+                    <div class="input-group-text">EUR</div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <label for="engine_volume">Об'єм двигуна</label>
+            <div class="input-group mb-2 mr-sm-2">
+                <input v-model="engineVolume" id="engine_volume" type="text" class="form-control">
+                <div class="input-group-append">
+                    <div class="input-group-text">см<sup>3</sup></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <manufacture-date-select v-model:age="carAge"/>
+        </div>
 
 
 
-        <h4>Акциз - {{ excise }}</h4>
-        <h4>Мито - {{ impost }}</h4>
-        <h4>ПДВ - {{ PDV }}</h4>
-        <hr>
-        <h4>Загальна ціна - {{ totalSum }}</h4>
+<!--        <h4>Акциз - {{ excise }}</h4>-->
+<!--        <h4>Мито - {{ impost }}</h4>-->
+<!--        <h4>ПДВ - {{ PDV }}</h4>-->
+<!--        <hr>-->
+<!--        <h4>Загальна ціна - {{ totalSum }}</h4>-->
     </div>
 </template>
 
 <script>
-import TextField from '@/components/Shared/TextField';
 import ManufactureDateSelect from "@/components/Shared/ManufactureDateSelect";
+import EngineTypeRadio from "@/components/Shared/EngineTypeRadio";
 
 export default {
     name: 'DieselCalculator',
     components: {
-        ManufactureDateSelect,
-        TextField
+        EngineTypeRadio,
+        ManufactureDateSelect
     },
     data: () => ({
+        engineType: '',
         engineVolume: '',
         carAge: 0,
         carPrice: 0,
